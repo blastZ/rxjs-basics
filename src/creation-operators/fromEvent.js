578e2fa1d1666517df8daac939ce717ea5b2d0ba
blastz <touchzh1996@outlook.com>
2021-07-07 22:23:11 +0800

    feat(app): add from event example

--- src/creation-operators/fromEvent.js
+++ src/creation-operators/fromEvent.js
@@ -2,4 +2,11 @@ import { fromEvent } from "rxjs";
 
 const click$ = fromEvent(document, "click");
 
-click$.subscribe((event) => console.log(event));
+const sub1 = click$.subscribe((val) => console.log("next", val));
+
+const sub2 = click$.subscribe((val) => console.log("next2", val));
+
+setTimeout(() => {
+  console.log("unsubscribing...");
+  sub2.unsubscribe();
+}, 3000);
